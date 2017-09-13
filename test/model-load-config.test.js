5a2e468a7f00e711ab8233754693cfe2e7997bd1
Riceball LEE <snowyu.lee@gmail.com>
2017-09-13 13:43:27 +0800

    + add the tests to support any configs format

---
+++ test/model-load-config.test.js
@@ -0,0 +1,45 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-boot
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+var boot = require('../');
+var fs = require('fs-extra');
+var path = require('path');
+var expect = require('chai').expect;
+var loadConfig = require('../lib/load-config');
+var yaml  = require('js-yaml');
+
+loadConfig.register(['.yaml', '.yml'], yaml.safeLoad);
+
+// add coffee-script to require.extensions
+require('coffee-script/register');
+
+var COFFEE_APP = path.join(__dirname, 'fixtures', 'coffee-app-2');
+
+describe('compiler', function() {
+  function getModelByName(aModels, aName) {
+    for (let model of aModels) {
+      if (model.name === aName) return model;
+    }
+  }
+  describe('from directory', function() {
+    it('loads Model yaml config files', function() {
+      var instructions = boot.compile(COFFEE_APP)
+      var model = getModelByName(instructions.models, 'Order');
+      expect(model).to.be.exist;
+      expect(model.sourceFile).to.be.exist;
+    });
+    it('loads component-config.yaml file', function() {
+      var instructions = boot.compile(COFFEE_APP)
+      var component = instructions.components[0];
+      expect(component).to.eql({
+        sourceFile: require.resolve('debug'),
+        config: {
+          option: 'value',
+        },
+      });
+    });
+  });
+});
+
